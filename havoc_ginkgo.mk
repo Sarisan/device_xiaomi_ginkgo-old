d59daef371855e5820851d9e418053a680a99557
Sarisan <sekoohaka.sarisan@gmail.com>
2020-08-11 09:00:50 +0300

    ginkgo: Havoc-OS bringup
    See https://github.com/Sarisan/local_manifests for dependencies
    
    Signed-off-by: Sarisan <sekoohaka.sarisan@gmail.com>

--- havoc_ginkgo.mk
+++ havoc_ginkgo.mk
@@ -12,16 +12,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_WIFI_EXT := true
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# GApps
+#WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+HAVOC_BUILD_TYPE=Official
+
+# MiuiCamera
 $(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_ginkgo
+PRODUCT_NAME := havoc_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
 PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="ginkgo" \
+    TARGET_DEVICE="ginkgo"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
